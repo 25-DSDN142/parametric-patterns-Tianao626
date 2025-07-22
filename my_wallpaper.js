925b5a55f15b1564397813fa86adf10c20bf9e8c
Tianao626 <wta18310329309@gmail.com>
2025-07-22 12:10:26 +1200

    Update my_wallpaper.js
    Try some curved graphics in PBA.

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -1,14 +1,16 @@
 //your parameter variables go here!
 let rect_width  = 20;
-let rect_height = 30;
-
+let rect_height = 60;
+let shape_Size  = 50;
+let shape_X     = 50;
+let shape_Y     = 50;
 
 function setup_wallpaper(pWallpaper) {
   pWallpaper.output_mode(DEVELOP_GLYPH);
-  //pWallpaper.output_mode(GRID_WALLPAPER);
+ //pWallpaper.output_mode(GRID_WALLPAPER);
   
   pWallpaper.resolution(FIT_TO_SCREEN);
-  pWallpaper.show_guide(true); //set this to false when you're ready to print
+  pWallpaper.show_guide(false); //set this to false when you're ready to print
 
   //Grid settings
   pWallpaper.grid_settings.cell_width  = 200;
@@ -17,9 +19,21 @@ function setup_wallpaper(pWallpaper) {
 }
 
 function wallpaper_background() {
-  background(240, 255, 240); //light honeydew green colour
+  background(141, 154, 155); //light honeydew green colour
 }
 
 function my_symbol() { // do not rename this function. Treat this similarly to a Draw function
-  rect(40 ,40, rect_width, rect_height);
+noStroke()
+bezier(0,100,50,0,150,200,200,100)
+fill(0)
+bezier(100,0,0,50,200,150,100,200)
+
+fill(0)
+ellipse(150,50,50,50)
+
+fill(255)
+ellipse(50,150,50,50)
+
+
+
 }
